73106eefe9892993ad0e2758c79dd5206e0eceb9
Charlie Precious R <161406505+bUgSqUisherlulu@users.noreply.github.com>
2024-10-23 18:27:00 +0200

    Add files via upload
    This project facilitates a student login system where individuals can enter their Registration Number and Password into a secure login form. Upon submission, the program authenticates the user's credentials. If verified, the system displays the student's Registration Number and Name. 
    
    In instances where a student is not registered, they are prompted to complete a Registration form, the details of which are then securely stored in the database. Following this process, the student is redirected to the login page to authenticate their account.
    
    Technologies utilized: MySQL Connector (to establish a connection between the Java application and XAMPP for PHPMyAdmin).
    
    Authentication details employed: (RegNum: "H230407R") (Password: "admin").

---
+++ Secure Coding/src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+    private JPanel MainPanel;
+    private JLabel lblreg;
+    private JLabel lblname;
+    public static void main(String[] args) {
+
+    }
+}
+
